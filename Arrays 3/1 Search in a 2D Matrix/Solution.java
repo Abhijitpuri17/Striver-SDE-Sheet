5c6b9356ceec716b0e5fff7d5065cc06fe1236e0
Abhijit Puri <85777554+Abhijitpuri17@users.noreply.github.com>
2022-06-13 19:27:00 +0530

    Day 3 Q 1 Search in a 2D matrix Java Solution

---
+++ Arrays 3/1 Search in a 2D Matrix/Solution.java
@@ -0,0 +1,21 @@
+import java.util.* ;
+import java.io.*; 
+import java.util.ArrayList;
+
+public class Solution {
+	static boolean findTargetInMatrix(ArrayList<ArrayList<Integer>> mat, int m, int n, int target) {
+		
+        int i = 0, j = n-1 ;
+        
+        while (i < m && j >= 0)
+        {
+            if (mat.get(i).get(j) == target) return true ;
+            
+            if (mat.get(i).get(j) < target) i++ ;
+            else j-- ;
+        }
+        
+        return false ;
+        
+	}
+}
