cd025945f9c34d280a197b12ae99bd310bd53280
Abhijit Puri <85777554+Abhijitpuri17@users.noreply.github.com>
2022-06-18 10:57:29 +0530

    Day 5 Q 2 Middle of Linked List Java Solution

---
+++ LinkedList 1/2 Middle of Linked List/Solution.java
@@ -0,0 +1,36 @@
+import java.util.* ;
+import java.io.*; 
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+       class Node 
+        { 
+        public:
+            int data;
+            Node next;
+            Node(int data) 
+            {
+              this.data = data;
+              this.next = null;
+            }
+        };
+
+*****************************************************************/
+
+public class Solution {
+    public static Node findMiddle(Node head) 
+    {
+        Node slow = head, fast = head;
+        
+        if (slow == null || slow.next == null) return slow;
+        
+       do{
+           slow = slow.next ;
+           fast = fast.next.next;
+       } while(fast != null && fast.next != null);
+        
+        return slow ;
+        
+    }
+}
