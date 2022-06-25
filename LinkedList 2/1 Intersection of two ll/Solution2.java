b36815b8511e6dc18e62f351d5903472736ea7b9
Abhijit Puri <85777554+Abhijitpuri17@users.noreply.github.com>
2022-06-26 00:17:03 +0530

    Update Solution2.java

--- LinkedList 2/1 Intersection of two ll/Solution2.java
+++ LinkedList 2/1 Intersection of two ll/Solution2.java
@@ -22,10 +22,78 @@ public class Solution {
         
         while (nodeA != nodeB)
         {
-            nodeA = nodeA.next == null ? secondHead : nodeA.next ;
-            nodeB = nodeB.next == null ? firstHead : nodeB.next ;
+            nodeA = nodeA == null ? secondHead : nodeA.next ;
+            nodeB = nodeB == null ? firstHead : nodeB.next ;
         }
         
         return nodeA.data ;
 	}
 }
+
+
+import java.util.* ;
+import java.io.*; 
+/************************************
+	 
+	 //Following is the class structure of the LinkedListNode class:
+	 
+	  class LinkedListNode<T> {
+		public T data;
+		public LinkedListNode<T> next;
+		
+		public LinkedListNode(T data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+
+*****************************************/
+public class Solution {
+
+	public static int findIntersection(LinkedListNode<Integer> firstHead, LinkedListNode<Integer> secondHead) {
+		LinkedListNode<Integer> nodeA = firstHead, nodeB = secondHead ;
+        
+        while (nodeA != nodeB)
+        {
+            nodeA = nodeA == null ? secondHead : nodeA.next ;
+            nodeB = nodeB == null ? firstHead : nodeB.next ;
+        }
+        
+        return nodeA.data ;
+	}
+}
+
+
+import java.util.* ;
+import java.io.*; 
+/************************************
+	 
+	 //Following is the class structure of the LinkedListNode class:
+	 
+	  class LinkedListNode<T> {
+		public T data;
+		public LinkedListNode<T> next;
+		
+		public LinkedListNode(T data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+
+*****************************************/
+public class Solution {
+
+	public static int findIntersection(LinkedListNode<Integer> firstHead, LinkedListNode<Integer> secondHead) {
+		LinkedListNode<Integer> nodeA = firstHead, nodeB = secondHead ;
+        
+        while (nodeA != nodeB)
+        {
+            nodeA = nodeA == null ? secondHead : nodeA.next ;
+            nodeB = nodeB == null ? firstHead : nodeB.next ;
+        }
+        
+        return nodeA.data ;
+	}
+}
+
+
