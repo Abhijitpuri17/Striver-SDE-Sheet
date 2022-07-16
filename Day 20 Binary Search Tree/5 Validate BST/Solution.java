0eadcbfeb683e3c2a8774378c4ad5c48acd4d198
Abhijit Puri <85777554+Abhijitpuri17@users.noreply.github.com>
2022-07-16 23:18:08 +0530

    Add: Day 20 Q 5 Validate BST Java Solution

---
+++ Day 20 Binary Search Tree/5 Validate BST/Solution.java
@@ -0,0 +1,35 @@
+/*************************************************************
+
+    Following is the Binary Tree node structure
+
+    class BinaryTreeNode<Integer> {
+        int data;
+        BinaryTreeNode<Integer> left;
+        BinaryTreeNode<Integer> right;
+
+        public BinaryTreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+ *************************************************************/
+
+public class Solution 
+{
+    public static boolean validateBST(BinaryTreeNode<Integer> root) {
+        return check(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    public static boolean check(BinaryTreeNode<Integer> node, long min, long max)
+    {
+        if (node == null)
+            return true ;
+        
+        if (node.data <= min || node.data >= max)
+            return false ;
+        
+        return check(node.left, min, node.data) && check(node.right, node.data, max) ;
+        
+    }
+    
+}
