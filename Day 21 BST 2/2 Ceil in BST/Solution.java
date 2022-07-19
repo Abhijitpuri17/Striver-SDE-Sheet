68d9badeb67c0b7f9782774b4b6069a8a73c4ac2
Abhijit Puri <85777554+Abhijitpuri17@users.noreply.github.com>
2022-07-20 00:49:58 +0530

    Add: Day 21 Q 2 Ceil in BST Java Solution

---
+++ Day 21 BST 2/2 Ceil in BST/Solution.java
@@ -0,0 +1,46 @@
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    class TreeNode<T>
+    {
+       public:
+        T data;
+        TreeNode<T> left;
+        TreeNode<T> right;
+
+        TreeNode(T data)
+        {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    };
+
+************************************************************/
+
+public class Solution {
+
+    public  static int findCeil(TreeNode<Integer> node, int x) {
+        int ans = -1 ;
+        
+        while (node != null)
+        {
+            if (node.data == x){
+                ans = x;
+                break ;
+            }
+            
+            if (node.data < x){
+                node = node.right ;
+            } else
+            {
+                ans = node.data ;
+                node = node.left ;
+            }
+            
+        }
+        
+        return ans ;
+    }
+}
